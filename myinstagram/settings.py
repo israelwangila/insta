b3174c967ab529c9b4d181734735859b127927ab
ow-tech <wagwanwekon@outloo.com>
2020-06-27 12:10:43 +0300

    st

--- myinstagram/settings.py
+++ myinstagram/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        # 'HOST': os.environ.get('DB_HOST'),
-        # 'PORT': os.environ.get('DB_PORT'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'TEST': {
             'NAME': 'test_instagram'
         }
