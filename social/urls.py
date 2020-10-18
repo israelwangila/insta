a969b967bc06b83e653967e35e3cc34ae6fe47fc
israelwangila <wangilaisrael@gmail.com>
2020-10-18 11:46:54 +0300

    creating urlpatterns url.py

--- social/urls.py
+++ social/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import url,include
 from django.contrib.auth import views 
 from django.contrib import admin
 
-
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'',include('instagram.urls'))
 ]
 
